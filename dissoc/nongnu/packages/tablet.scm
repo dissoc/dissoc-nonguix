e805732ecc6e274fdd37a3dc538cd357383f8558
Justin Bishop <mail@dissoc.me>
2024-02-14 04:51:28 -0600

    add package: digimend-module-linux

---
+++ dissoc/nongnu/packages/tablet.scm
@@ -0,0 +1,6 @@
+;;; Copyright © 2024 Justin Bishop <mail@dissoc.me>
+
+(define-module (dissoc nongnu packages tablet)
+  #:use-module (dissoc gnu packages tablet))
+
+(define-public digimend-module-linux (make-digimend linux))
