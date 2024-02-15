1e5d84b482e6637cf76817aa0ce8a949af1db288
Justin Bishop <mail@dissoc.me>
2024-02-15 14:25:10 -0600

    Update repo dependency and module fix
    added: nonguix dependency to repository
    fix: added use-module linux to tablet module

--- dissoc/nongnu/packages/tablet.scm
+++ dissoc/nongnu/packages/tablet.scm
@@ -1,6 +1,7 @@
 ;;; Copyright © 2024 Justin Bishop <mail@dissoc.me>
 
 (define-module (dissoc nongnu packages tablet)
-  #:use-module (dissoc gnu packages tablet))
+  #:use-module (dissoc gnu packages tablet)
+  #:use-module (nongnu packages linux))
 
 (define-public digimend-module-linux (make-digimend linux))
